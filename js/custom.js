c0451e7c6c7d7ab67e379ab5abd0daf352b1c2d6
Luke Simonson <lukephilipsimonson@gmail.com>
2020-10-01 21:58:11 -0500

    required captcha

--- js/custom.js
+++ js/custom.js
@@ -42,3 +42,19 @@ $(function(){
 
 });
 
+document.getElementById("mG61Hd").addEventListener("submit",function(evt)
+  {
+  
+  var response = grecaptcha.getResponse();
+  if(response.length == 0) 
+  { 
+    //reCaptcha not verified
+    alert("please verify you are humann!"); 
+    evt.preventDefault();
+    return false;
+  }
+  //captcha verified
+  //do the rest of your validations here
+  
+});
+
